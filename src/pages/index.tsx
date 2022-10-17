fd5d8b7ecd52950bd7b1ecae3eeaef4b19e5a530
william andrade de souza <williamstrayder@gmail.com>
2022-10-17 01:40:22 -0300

    :sparkles: feat: add component Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600">
-      <span className="text-4xl">Texto</span>
+    <div
+      className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 to-blue-600
+      text-white
+    `}
+    >
+      <Layout title="Cadastro Simples">
+        <span>conteudo</span>
+      </Layout>
     </div>
   );
 }
