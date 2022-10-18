5df1ccd2ea9022b6fb840a35d4bc8cfa45125a79
william andrade de souza <williamstrayder@gmail.com>
2022-10-18 11:45:39 -0300

    :sparkles: feat: add core Client

---
+++ src/core/Client.ts
@@ -0,0 +1,27 @@
+export default class Client {
+  #id: string | null;
+  #name: string;
+  #age: number;
+
+  constructor(name: string, age: number, id: string | null = null) {
+    this.#name = name;
+    this.#age = age;
+    this.#id = id;
+  }
+
+  static vazio() {
+    return new Client("", 0);
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get idade() {
+    return this.#age;
+  }
+}
