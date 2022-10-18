5bb0df3f17749f13f43d4118e5e0f1f198373868
william andrade de souza <williamstrayder@gmail.com>
2022-10-18 18:24:47 -0300

    :sparkles: feat: add component Table

---
+++ src/components/Table.tsx
@@ -0,0 +1,88 @@
+import Client from "../core/Client";
+import { IconEdit, IconTrash } from "./Icons";
+
+interface TableProps {
+  clients: Client[];
+  clientSelected?: (client: Client) => void;
+  clientExcluded?: (client: Client) => void;
+}
+
+export default function Table(props: TableProps) {
+  const showActions = props.clientSelected || props.clientExcluded;
+
+  function renderHeader() {
+    return (
+      <tr>
+        <th className="text-left p-4">Code</th>
+        <th className="text-left p-4">Name</th>
+        <th className="text-left p-4">Age</th>
+        {showActions && <th className="p-4">Actions</th>}
+      </tr>
+    );
+  }
+
+  function renderData() {
+    return props.clients?.map((client, i) => {
+      return (
+        <tr
+          key={client.id}
+          className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
+          {showActions && renderActions(client)}
+        </tr>
+      );
+    });
+  }
+
+  function renderActions(client: Client) {
+    return (
+      <td className="flex justify-center">
+        {props.clientSelected ? (
+          <button
+            onClick={() => props.clientSelected?.(client)}
+            className={`
+          flex justify-center items-center
+          text-green-600 rounded-full p-2 m-1
+          hover:bg-purple-50
+        `}
+          >
+            {IconEdit}
+          </button>
+        ) : (
+          false
+        )}
+        {props.clientExcluded ? (
+          <button
+            onClick={() => props.clientExcluded?.(client)}
+            className={`
+          flex justify-center items-center
+          text-red-500 rounded-full p-2 m-1
+          hover:bg-purple-50
+        `}
+          >
+            {IconTrash}
+          </button>
+        ) : (
+          false
+        )}
+      </td>
+    );
+  }
+
+  return (
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`
+        text-gray-100
+      bg-gradient-to-r from-purple-500 to-purple-800
+      `}
+      >
+        {renderHeader()}
+      </thead>
+      <tbody>{renderData()}</tbody>
+    </table>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client("Maria", 30, "1"),
+    new Client("Bia", 46, "2"),
+    new Client("william", 46, "3"),
+    new Client("Pedro", 46, "4"),
+  ];
+
+  function clientExcluded(client: Client) {
+    console.log("Excluded", client.name);
+  }
+  function clientSelected(client: Client) {
+    console.log("Selected", client.name);
+  }
   return (
     <div
       className={`
@@ -10,7 +25,11 @@ export default function Home() {
     `}
     >
       <Layout title="Cadastro Simples">
-        <span>conteudo</span>
+        <Table
+          clients={clients}
+          clientExcluded={clientExcluded}
+          clientSelected={clientSelected}
+        ></Table>
       </Layout>
     </div>
   );
